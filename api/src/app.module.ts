7e64bf29a479fdc0ae3abcc5567249144f70683e
James Riall <jamesrariall@gmail.com>
2022-06-04 15:25:46 +0100

    Add habit resource

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -7,12 +7,14 @@ import { AppService } from "./app.service";
 import { getEnvPath } from "./common/helper/env.helper";
 import { TypeOrmConfigService } from "./shared/typeorm/typeorm.service";
 import { UsersModule } from "./users/users.module";
+import { HabitsModule } from "./habits/habits.module";
 
 const envFilePath: string = getEnvPath(`${__dirname}/common/envs`);
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    HabitsModule,
     TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
     UsersModule,
   ],
---
+++ api/src/habits/entities/habit.entity.ts
@@ -0,0 +1,23 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+export class Habit {
+  @PrimaryGeneratedColumn()
+  public id!: number;
+
+  @Column({ type: "varchar", length: 120 })
+  public name: string;
+
+  // Create and Update Date Columns
+  @CreateDateColumn({ type: "timestamp" })
+  public createdAt!: Date;
+
+  @UpdateDateColumn({ type: "timestamp" })
+  public updatedAt!: Date;
+}
